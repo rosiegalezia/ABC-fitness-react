8853fc17ecdde78dd61ebcfb85137039c3b92a48
rosiegalezia <rosegalezia@gmail.com>
2024-02-06 18:05:40 +0000

    going to try using react-scheduler

--- react/src/Components/Class.jsx
+++ react/src/Components/Class.jsx
@@ -3,7 +3,7 @@ function Class(props) {
         <div className="card">
             <h2>{props.name}</h2>
             <p>{props.description}</p>
-            <img src={props.image}/>
+            {/* <img src={props.image}/> */}
             <p>{props.instructor}</p>
             <p>{props.duration}</p>
         </div>
--- react/src/Pages/Timetable.jsx
+++ react/src/Pages/Timetable.jsx
@@ -1,7 +1,54 @@
+import classes from '../assets/ClassData/classes.json'
+import class from '../Components/Class.jsx'
+
+import TimeTable from "react-timetable-events";
+
 function Timetable() {
     // Body
     return <div>
         <h2 className="p-5">View our class timetable below</h2>
+
+        
+
+        {/* <div className="App">
+            <table>
+                <tr>
+                    <th></th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th>Sunday</th>
+                </tr>
+
+                <tr>
+                    <th>morning</th>
+                    <td>class 1</td>
+                </tr>
+
+                <tr>
+                    <th>afternoon</th>
+                    <td>class 2</td>
+                </tr>
+
+                <tr>
+                    <th>evening</th>
+                    <td>class 3</td>
+                </tr>
+                {/* {classData.map((val, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{val.name}</td>
+                            <td>{val.description}</td>
+                            <td>{val.instructor}</td>
+                        </tr>
+                    )
+                })} */}
+            </table>
+        </div> */}
+
     </div>
 }
 
