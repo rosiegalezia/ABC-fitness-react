8b2070627f360b6228f479c5e6f5470ebb8bbc6c
rosiegalezia <rosegalezia@gmail.com>
2024-02-07 14:16:49 +0000

    added images to all the classes but need to fix responsive sizing

--- src/Components/Class.jsx
+++ src/Components/Class.jsx
@@ -3,11 +3,11 @@ function Class(props) {
         <div className="card m-3 col-12 col-xl-3 col-lg-3 col-md-4 col-sm-12">
 
             <div className="card-body p-3">
-            <h2>{props.name}</h2>
-            <p>{props.description}</p>
-            <img src={props.image} />
-            <p>{props.instructor}</p>
-            <p>{props.duration}</p>
+                <h2>{props.name}</h2>
+                <p>{props.description}</p>
+                <img src={props.image} width={400}/>
+                <p>with {props.instructor}</p>
+                <p>{props.duration}</p>
             </div>
 
             <div className="card-footer">
