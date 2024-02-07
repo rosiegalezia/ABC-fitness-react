af6128b0d017256a1bd8f98701b7faf1481c22d4
rosiegalezia <rosegalezia@gmail.com>
2024-02-07 14:30:11 +0000

    added styling to 'browse classes' cards

--- src/Components/Class.jsx
+++ src/Components/Class.jsx
@@ -1,18 +1,17 @@
 function Class(props) {
     return (
-        <div className="card m-3 col-12 col-xl-3 col-lg-3 col-md-4 col-sm-12">
+        <div className="card m-4 col-12 col-xl-3 col-lg-3 col-md-4 col-sm-12">
 
-            <div className="card-body p-3">
+            <img className="card-img-top" src={props.image} />
+            <div className="card-body pb-2 p-4">
                 <h2>{props.name}</h2>
                 <p>{props.description}</p>
-                <img src={props.image} width={400}/>
-                <p>with {props.instructor}</p>
+                {/* <p>with {props.instructor}</p> */}
                 <p>{props.duration}</p>
             </div>
-
-            <div className="card-footer">
-                <button className="bookBtn">Book</button>
-                <button className="saveBtn">Save</button>
+            <div className="card-footer pt-1 p-4">
+                <button className="btn bookBtn m-1">Book</button>
+                <button className="btn saveBtn m-1">Save</button>
             </div>
 
         </div>
--- src/Pages/BrowseClasses.jsx
+++ src/Pages/BrowseClasses.jsx
@@ -6,6 +6,7 @@ import '../assets/styles.css'
 function BrowseClasses() {
     return <div className="">
         <h2 className="page-heading p-5 pb-1">Browse our classes</h2>
+        <h4 className="subheading px-5 pt-2 pb-1">Whether you're a pilates pro or total beginner, we've got the class for you!</h4>
         <div className="flex-wrap d-flex m-5">
             {fitnessclasses.map((fitnessclass) => {
                 return <Class key={fitnessclass.id} name={fitnessclass.name} image={fitnessclass.image} description={fitnessclass.description} instructor={fitnessclass.instructor} duration={fitnessclass.duration} />;
