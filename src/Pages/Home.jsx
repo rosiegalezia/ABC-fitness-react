a851978fba52d229d49bac60d2bde4e8294b5766
rosiegalezia <rosegalezia@gmail.com>
2024-02-11 16:24:30 +0000

    going to play with bootstrap cols to make timetable

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ function Home() {
     return <div className="page-heading p-5 pb-1">
         <h2 className="">Welcome to ABC Fitness Studio</h2>
         <h4 className="subheading">Begin your health journey with us, it's as easy as A, B, C!</h4>
+        <div></div>
     </div>
 }
 
--- src/Pages/Timetable.jsx
+++ src/Pages/Timetable.jsx
@@ -19,60 +19,79 @@ function Timetable() {
 
     // Body
     return <div className='timetable'>
-        
+
         <div className="page-heading p-5 pb-1">
             <h2 className="">View our class timetable below</h2>
             <h4 className="subheading">Find your perfect class, at the perfect time.</h4>
         </div>
 
         <div className="container m-5">
-                <table className="table">
-                    <thead>
-                        <tr className=''>
-                            <th></th>
+            <table className="table">
+{/* 
+                <div>
+                    <div className='col-6'>
+                        <div className='row'>
                             <th>Monday</th>
+                            <tr>Class 1</tr>
+                        </div>
+                    </div>
+                </div>
+                
+                <div>
+                    <div className='col-6'>
+                        <div className='row'>
                             <th>Tuesday</th>
-                            <th>Wednesday</th>
-                            <th>Thursday</th>
-                            <th>Friday</th>
-                            <th>Saturday</th>
-                            <th>Sunday</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>Morning</th>
-                            {classCells[0]}
-                            {classCells[2]}
-                            {classCells[9]}
-                            {classCells[7]}
-                            {classCells[1]}
-                            {classCells[3]}
-                            {classCells[8]}
-                        </tr>
-                        <tr>
-                            <th>Afternoon</th>
-                            {classCells[1]}
-                            {classCells[3]}
-                            {classCells[4]}
-                            {classCells[5]}
-                            {classCells[6]}
-                            {classCells[10]}
-                            {classCells[9]}
-                        </tr>
-                        <tr>
-                            <th>Evening</th>
-                            {classCells[4]}
-                            {classCells[1]}
-                            {classCells[10]}
-                            {classCells[9]}
-                            {classCells[7]}
-                            {classCells[6]}
-                            {classCells[3]}
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                            <tr>Class 2</tr>
+                        </div>
+                    </div>
+                </div> */}
+
+                <thead>
+                    <tr className=''>
+                        <th></th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                        <th>Sunday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Morning</th>
+                        {classCells[0]}
+                        {classCells[2]}
+                        {classCells[9]}
+                        {classCells[7]}
+                        {classCells[1]}
+                        {classCells[3]}
+                        {classCells[8]}
+                    </tr>
+                    <tr>
+                        <th>Afternoon</th>
+                        {classCells[1]}
+                        {classCells[3]}
+                        {classCells[4]}
+                        {classCells[5]}
+                        {classCells[6]}
+                        {classCells[10]}
+                        {classCells[9]}
+                    </tr>
+                    <tr>
+                        <th>Evening</th>
+                        {classCells[4]}
+                        {classCells[1]}
+                        {classCells[10]}
+                        {classCells[9]}
+                        {classCells[7]}
+                        {classCells[6]}
+                        {classCells[3]}
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
     </div>
 }
